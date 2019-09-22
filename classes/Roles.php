b7a6bf93958b6b6a7136c1ef5d6b098c3a3c0e5b
Echo G <ChaosExAnima@users.noreply.github.com>
2019-09-22 13:01:56 -0400

    Adds Safety role.

--- classes/Roles.php
+++ classes/Roles.php
@@ -9,7 +9,7 @@ namespace EventVetting;
 
 class Roles {
 	/**
-	 * Name of new role.
+	 * Name of vetter role.
 	 *
 	 * @var string
 	 */
@@ -23,24 +23,55 @@ class Roles {
 	const VETTER_CAP = 'review_event_applications';
 
 	/**
-	 * Adds role if it doesn't exist already.
+	 * Name of safety role.
+	 *
+	 * @var string
+	 */
+	const SAFETY_ROLE = 'event_safety';
+
+	/**
+	 * Safety capability name.
+	 *
+	 * @var string
+	 */
+	const SAFETY_CAP = 'review_event_safety';
+
+	/**
+	 * Installs roles.
 	 *
 	 * @return void
 	 */
 	public function init() {
 		if ( ! get_role( self::VETTER_ROLE ) ) {
 			$subscriber = get_role( 'subscriber' );
-			add_role(
-				self::VETTER_ROLE,
-				__( 'Vetter', 'event-vetting' ),
-				array_merge( $subscriber->capabilities, [
-					self::VETTER_CAP       => true,
-					'view_admin_dashboard' => true,
-				] )
-			);
+		$base_caps  = $subscriber->capabilities;
+
+		// Adds capability to view dashboard if WC is installed.
+		if ( defined( 'WOOCOMMERCE_VERSION' ) ) {
+			$base_caps['view_admin_dashboard'] = true;
 		}
 
+		delete_role( self::VETTER_ROLE );
+			add_role(
+				self::VETTER_ROLE,
+			__( 'Event Vetter', 'event-vetting' ),
+			array_merge( $base_caps, [
+				self::VETTER_CAP => true,
+			] )
+		);
+
+		delete_role( self::SAFETY_ROLE );
+		add_role(
+			self::SAFETY_ROLE,
+			__( 'Event Safety', 'event-vetting' ),
+			array_merge( $base_caps, [
+				self::SAFETY_ROLE => true,
+				] )
+			);
+
+		// Updates admin with all capabilities.
 		$admin = get_role( 'administrator' );
 		$admin->add_cap( self::VETTER_CAP );
+		$admin->add_cap( self::SAFETY_CAP );
 	}
 }
